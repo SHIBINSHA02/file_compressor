5677385a181b4ef70ca7a46323279afdf03df45a
SHIBINSHA <shibin24888@gmail.com>
2023-11-04 19:32:46 +0530

    structured

---
+++ main.py
@@ -0,0 +1,62 @@
+import zlib
+import base64
+
+with open("test.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+
+print("Original Text:")
+print(text)
+
+
+try:
+    with open("comp.txt", "r") as fc:
+        existing_data = fc.read()
+    if existing_data:
+        # If there is text in "comp.txt", don't overwrite it
+        print("File 'comp.txt' already contains data. Skipping compression and writing.")
+    else:
+        # Compress the text and encode it in base64
+        compressed_data = zlib.compress(text.encode("utf-8"), 9)  # Use compression level 9 for better compression
+        base64_encoded_data = base64.b64encode(compressed_data).decode("utf-8")
+
+        print("Compressed and Base64 Encoded Data:")
+        print(base64_encoded_data)
+
+        # Write the compressed and encoded data to the file
+        with open("comp.txt", "w") as fc:
+            fc.write(base64_encoded_data)
+
+        with open("comp.txt", "r") as fc:
+            data=fc.read()
+            compressed_data = base64.b64decode(data)
+
+            # Then, decompress using zlib
+            decompressed_text = zlib.decompress(compressed_data).decode("utf-8")
+
+            # Now, `decompressed_text` contains the original text that was compressed and encoded
+            print(decompressed_text)
+        
+
+except FileNotFoundError:
+    compressed_data = zlib.compress(text.encode("utf-8"), 9) 
+    base64_encoded_data = base64.b64encode(compressed_data).decode("utf-8")
+
+    print("Compressed and Base64 Encoded Data:")
+    print(base64_encoded_data)
+
+  
+    with open("comp.txt", "w") as fc:
+        fc.write(base64_encoded_data)
+        
+    with open("comp.txt", "r") as fc:
+        data=fc.read()
+        compressed_data = base64.b64decode(data)
+
+        # Then, decompress using zlib
+        decompressed_text = zlib.decompress(compressed_data).decode("utf-8")
+
+        # Now, `decompressed_text` contains the original text that was compressed and encoded
+        print(decompressed_text)
+        
+
